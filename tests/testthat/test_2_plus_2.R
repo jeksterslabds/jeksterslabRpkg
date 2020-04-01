56925fb3ea552ddac4a614ed70729186cea64af7
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-04-01 11:21:52 +0800

    BUILD.

--- tests/testthat/test_2_plus_2.R
+++ tests/testthat/test_2_plus_2.R
@@ -6,11 +6,12 @@
 #'   rmarkdown::github_document:
 #'     toc: true
 #' ---
-
+#'
 #+ setup
 library(testthat)
 library(jeksterslabRpkg)
-
+context("Test 2 + 2.")
+#'
 #+ test that_01, echo=TRUE
 test_that("2 + 2 = 4", {
   expect_equivalent(
