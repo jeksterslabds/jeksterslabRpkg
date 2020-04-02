8082c75ff3a78202ad6637e36fa8a8324f928818
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-04-02 23:14:48 +0800

    Added working file of pkg_gitlsf.R

---
+++ R/pkg_gitlsf.R
@@ -0,0 +1,33 @@
+pkg_gitlsf <- function(ext = c(
+    "gz",
+    "zip",
+    "Rds",
+    "rds",
+    "Rda",
+    "rda")) {
+          if (is.null(pkg_root)) {
+    pkg_root <- getwd()
+  }
+  if (!file.exists(
+    file.path(
+      pkg_root,
+      "DESCRIPTION"
+    )
+  )
+  ) {
+    stop("Not a valid package root directory.\n")
+  }
+  output <- sort(
+    paste0(
+      "*.",
+      ext,
+      " filter=lfs diff=lfs merge=lfs -text"
+    )
+  )
+  # add script to check if .gitattributes exists first and append line by line
+  util_txt2file(
+    text = output,
+    dir = pkg_root,
+    fn = ".gitattributes"
+  )
+}
