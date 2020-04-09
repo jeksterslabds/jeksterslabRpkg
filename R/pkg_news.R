c84ed94d2c6f464d000a04db95365a45b68f48cf
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-04-09 12:34:59 +0800

    Added function to generate sample NEWS.md.

---
+++ R/pkg_news.R
@@ -0,0 +1,43 @@
+#' Create a Boilerplate Package NEWS.md File.
+#'
+#' Creates a boilerplate package NEWS.md file.
+#'
+#' Note that if [jeksterslabRpkg::pkg_create()] is used,
+#' this function will be called.
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#' @inheritParams pkg_rbuildignore
+#' @examples
+#' \dontrun{
+#' pkg_news(
+#'   pkg_root = "~/boilerplatePackage"
+#' )
+#' }
+#' @export
+pkg_news <- function(pkg_root,
+                         msg = "NEWS.md file path:") {
+  pkg_name <- basename(pkg_root)
+  output <- readLines(
+    con = system.file(
+      "extdata",
+      "NEWS",
+      package = "jeksterslabRpkg",
+      mustWork = TRUE
+    )
+  )
+  output <- gsub(
+    pattern = "BOILERPLATEPACKAGE",
+    replacement = pkg_name,
+    x = output
+  )
+  output <- paste0(
+    output,
+    collapse = "\n"
+  )
+  util_txt2file(
+    text = output,
+    dir = root_vignettes,
+    fn = "NEWS.md",
+    msg = msg
+  )
+}
