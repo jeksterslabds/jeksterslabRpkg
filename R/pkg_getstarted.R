1835b8e02fe7415747c7aaca402dda6dc834a9bf
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-04-09 12:34:29 +0800

    Added getstarted.
    This generates a vignette file that is used as the Get Started page in pkgdown.

---
+++ R/pkg_getstarted.R
@@ -0,0 +1,47 @@
+#' Create a Boilerplate Package Get Started Vignette.
+#'
+#' Creates a boilerplate package Get Started vignette.
+#'
+#' Note that if [jeksterslabRpkg::pkg_create()] is used,
+#' this function will be called.
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#' @inheritParams pkg_rbuildignore
+#' @examples
+#' \dontrun{
+#' pkg_getstarted(
+#'   pkg_root = "~/boilerplatePackage"
+#' )
+#' }
+#' @export
+pkg_getstarted <- function(pkg_root,
+                         msg = "Get started file path:") {
+  pkg_name <- basename(pkg_root)
+  root_vignettes <- file.path(
+    pkg_root,
+    "vignettes"
+  )
+  output <- readLines(
+    con = system.file(
+      "extdata",
+      "getstarted",
+      package = "jeksterslabRpkg",
+      mustWork = TRUE
+    )
+  )
+  output <- gsub(
+    pattern = "BOILERPLATEPACKAGE",
+    replacement = pkg_name,
+    x = output
+  )
+  output <- paste0(
+    output,
+    collapse = "\n"
+  )
+  util_txt2file(
+    text = output,
+    dir = root_vignettes,
+    fn = paste0(pkg_name, ".Rmd"),
+    msg = msg
+  )
+}
