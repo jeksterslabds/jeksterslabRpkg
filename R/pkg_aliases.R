4f1a62c9a5437265bc336d5e694ea40c471ec6a6
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-05-01 03:44:12 +0800

    Added documentation.

--- R/pkg_aliases.R
+++ R/pkg_aliases.R
@@ -1,30 +1,36 @@
-#' Aliases
+#' Aliases styler::style_dir()
 #'
-#' @author Ivan Jacob Agaloos Pesigan
 #' @export
 rsty <- function() {
   styler::style_dir()
 }
+#' Aliases styler::style_pkg()
+#'
+#' @export
 rspkg <- function() {
   styler::style_pkg()
 }
+#' Aliases devtools::test()
+#'
+#' @export
 rtest <- function() {
   devtools::test()
 }
+#' Aliases devtools::check()
+#'
+#' @export
 rchk <- function() {
   devtools::check()
 }
-rchk <- function() {
-  jeksterslabRpkg::pkg_build()
-}
-rpkg <- function() {
-  styler::style_pkg()
-  devtools::check()
-  devtools::install()
-}
+#' Aliases pkg_build()
+#'
+#' @export
 rbuild <- function() {
   pkg_build()
 }
+#' Aliases styler::style_pkg(); devtools::check(); devtools::install()
+#'
+#' @export
 rpkg <- function() {
   styler::style_pkg()
   devtools::check()
