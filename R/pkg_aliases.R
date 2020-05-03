3bfb59115d39e899683d73780b439277be588bb0
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-05-04 02:46:55 +0800

    Edits to aliases.

--- R/pkg_aliases.R
+++ R/pkg_aliases.R
@@ -1,69 +1,231 @@
-#' Aliases jeksterslabRpkg::util_render() Recursive and Parallel
+#' Alias for jeksterslabRutils::util_render()
 #'
+#' Alias for [`jeksterslabRutils::util_render()`]
+#' with the following arguments:
+#' - `dir = getwd()`
+#' - `recursive = TRUE`
+#' - `files = NULL`
+#' - `par = TRUE`
+#' - `ncores = NULL`
+#'
+#' @inheritParams jeksterslabRutils::util_render
+#' @seealso [`jeksterslabRutils::util_render()`]
 #' @export
-rpkg <- function() {
-  jeksterslabRpkg::util_render(
-    dir = getwd(),
-    recursive = TRUE,
-    files = NULL,
-    par = TRUE,
-    ncores = NULL
+rrender <- function(dir = getwd(),
+                    recursive = TRUE,
+                    files = NULL,
+                    par = TRUE,
+                    ncores = NULL) {
+  jeksterslabRutils::util_render(
+    dir = dir,
+    recursive = recursive,
+    files = files,
+    par = par,
+    ncores = ncores
   )
 }
-#' Aliases styler::style_dir()
+
+#' Alias for styler::style_dir()
 #'
+#' @inheritParams styler::style_dir
+#' @seealso [`styler::style_dir()`]
 #' @export
-rsty <- function() {
-  styler::style_dir()
+rsty <- function(path = ".",
+                 ...,
+                 style = styler::tidyverse_style,
+                 transformers = style(...),
+                 filetype = c("R", "Rprofile"),
+                 recursive = TRUE,
+                 exclude_files = NULL,
+                 exclude_dirs = c("packrat", "renv"),
+                 include_roxygen_examples = TRUE) {
+  styler::style_dir(
+    path = path,
+    ...,
+    style = style,
+    transformers = transformers,
+    filetype = filetype,
+    recursive = recursive,
+    exclude_files = exclude_files,
+    exclude_dirs = exclude_dirs,
+    include_roxygen_examples = include_roxygen_examples
+  )
 }
 #' Aliases styler::style_pkg()
 #'
+#' @inheritParams styler::style_pkg
+#' @seealso [`styler::style_pkg()`]
 #' @export
-rspkg <- function() {
-  styler::style_pkg()
+rspkg <- function(pkg = ".",
+                  ...,
+                  style = styler::tidyverse_style,
+                  transformers = style(...),
+                  filetype = c("R", "Rprofile"),
+                  exclude_files = "R/RcppExports.R",
+                  exclude_dirs = c("packrat", "renv"),
+                  include_roxygen_examples = TRUE) {
+  styler::style_pkg(
+    pkg = pkg,
+    ...,
+    style = style,
+    transformers = transformers,
+    filetype = filetype,
+    exclude_files = exclude_files,
+    exclude_dirs = exclude_dirs,
+    include_roxygen_examples = include_roxygen_examples
+  )
 }
-#' Aliases jeksterslabRpkg::util_style() Recursive and Parallel
+
+#' Alias for jeksterslabRutils::util_style()
 #'
-#' `jeksterslabRpkg::util_style(
-#'   dir = getwd(),
-#'   recursive = TRUE,
-#'   par = TRUE,
-#'   ncores = NULL
-#' )`
+#' Alias for [`jeksterslabRutils::util_style()`]
+#' with the following arguments:
+#' - `dir = getwd()`
+#' - `recursive = TRUE`
+#' - `par = TRUE`
+#' - `ncores = NULL`
 #'
-#' @inheritParams util_style
+#' @inheritParams jeksterslabRutils::util_style
+#' @seealso [`jeksterslabRutils::util_style()`]
 #' @export
 rstyle <- function(dir = getwd(),
                    recursive = TRUE,
                    par = TRUE,
                    ncores = NULL) {
-  jeksterslabRpkg::util_style(
+  jeksterslabRutils::util_style(
     dir = dir,
     recursive = recursive,
     par = par,
     ncores = ncores
   )
 }
-#' Aliases devtools::test()
+
+#' Alias for devtools::test()
 #'
+#' @inheritParams devtools::test
+#' @seealso [`devtools::test()`]
 #' @export
-rtest <- function() {
-  devtools::test()
+rtest <- function(pkg = ".",
+                  filter = NULL,
+                  stop_on_failure = FALSE,
+                  export_all = TRUE,
+                  ...) {
+  devtools::test(
+    pkg = pkg,
+    filter = filter,
+    stop_on_failure = stop_on_failure,
+    export_all = export_all,
+    ...
+  )
 }
-#' Aliases devtools::check()
+#' Alias for devtools::check()
 #'
+#' @inheritParams devtools::check
+#' @seealso [`devtools::check()`]
 #' @export
-rchk <- function() {
-  devtools::check()
+rchk <- function(pkg = ".",
+                 document = NA,
+                 build_args = NULL,
+                 ...,
+                 manual = FALSE,
+                 cran = TRUE,
+                 remote = FALSE,
+                 incoming = remote,
+                 force_suggests = FALSE,
+                 run_dont_test = FALSE,
+                 args = "--timings",
+                 env_vars = c(NOT_CRAN = "true"),
+                 quiet = FALSE,
+                 check_dir = tempdir(),
+                 cleanup = TRUE,
+                 vignettes = TRUE,
+                 error_on = c("never", "error", "warning", "note")) {
+  devtools::check(
+    pkg = pkg,
+    document = document,
+    build_args = build_args,
+    ...,
+    manual = manual,
+    cran = cran,
+    remote = remote,
+    incoming = incoming,
+    force_suggests = force_suggests,
+    run_dont_test = run_dont_test,
+    args = args,
+    env_vars = env_vars,
+    quiet = quiet,
+    check_dir = check_dir,
+    cleanup = cleanup,
+    vignettes = vignettes,
+    error_on = error_on
+  )
 }
-#' Aliases pkg_build()
+
+#' Alias for jeksterslabRpkg::pkg_build()
 #'
+#' Alias for [`jeksterslabRpkg::pkg_build()`]
+#' with the following arguments:
+#' - `pkg_root = NULL`
+#' - `minimal = FALSE`
+#' - `style = TRUE`
+#' - `data = TRUE`
+#' - `render = TRUE`
+#' - `readme = TRUE`
+#' - `vignettes = TRUE`
+#' - `tests = TRUE`
+#' - `pkgdown = TRUE`
+#' - `par = TRUE`
+#' - `ncores = NULL`
+#' - `git = FALSE`
+#' - `github = FALSE`
+#' - `commit_msg = "BUILD`
+#'
+#' @inheritParams pkg_build
+#' @seealso [`jeksterslabRpkg::pkg_build()`]
 #' @export
-rbuild <- function() {
-  pkg_build()
+rbuild <- function(pkg_root = NULL,
+                   minimal = FALSE,
+                   style = TRUE,
+                   data = TRUE,
+                   render = TRUE,
+                   readme = TRUE,
+                   vignettes = TRUE,
+                   tests = TRUE,
+                   pkgdown = TRUE,
+                   par = TRUE,
+                   ncores = NULL,
+                   git = FALSE,
+                   github = FALSE,
+                   commit_msg = "BUILD") {
+  jeksterslabRpkg::pkg_build(
+    pkg_root = pkg_root,
+    minimal = minimal,
+    style = style,
+    data = data,
+    render = render,
+    readme = readme,
+    vignettes = vignettes,
+    tests = tests,
+    pkgdown = pkgdown,
+    par = par,
+    ncores = ncores,
+    git = git,
+    github = github,
+    commit_msg = commit_msg
+  )
 }
-#' Aliases styler::style_pkg(); devtools::check(); devtools::install()
+
+#' Alias for styler::style_pkg(); devtools::check(); devtools::install()
 #'
+#' Alias for
+#' [`styler::style_pkg()`],
+#' [`devtools::check()`],
+#' [`devtools::install()`]
+#' with default arguments.
+#'
+#' @seealso [`styler::style_pkg()`]
+#' @seealso [`devtools::check()`]
+#' @seealso [`devtools::install()`]
 #' @export
 rpkg <- function() {
   styler::style_pkg()
