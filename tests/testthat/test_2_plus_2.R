9f9e744d0123ec5b94c93996c53137218d731b51
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-05-07 02:16:46 +0800

    Fixed yml

--- tests/testthat/test_2_plus_2.R
+++ tests/testthat/test_2_plus_2.R
@@ -1,17 +1,27 @@
 #' ---
-#' title: "Test: 2 + 2"
+#' title: "Test: test_2_plus_2"
 #' author: "Ivan Jacob Agaloos Pesigan"
 #' date: "`r Sys.Date()`"
-#' output:
-#'   rmarkdown::github_document:
-#'     toc: true
+#' output: rmarkdown::html_vignette
+#' vignette: >
+#'   %\VignetteIndexEntry{Test: test_2_plus_2}
+#'   %\VignetteEngine{knitr::rmarkdown}
+#'   %\VignetteEncoding{UTF-8}
 #' ---
+#'
+#+ include=FALSE, cache=FALSE
+knitr::opts_chunk$set(
+  error = TRUE,
+  collapse = TRUE,
+  comment = "#>",
+  out.width = "100%"
+)
 #'
 #+ setup
 library(testthat)
-context("Test 2 + 2.")
+context("Test test_2_plus_2.")
 #'
-#+ test that_01, echo=TRUE
+#+ testthat, echo=TRUE
 test_that("2 + 2 = 4", {
   expect_equivalent(
     2 + 2,
