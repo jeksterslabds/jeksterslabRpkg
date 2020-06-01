5cec1d7ce8b7bc332ea23203349ea2d6062e5ec2
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-06-01 09:21:07 +0800

    Created pkg_style

---
+++ R/pkg_style.R
@@ -0,0 +1,37 @@
+#' Style `R` and `R` Markdown Files in the Package Directory.
+#'
+#' Styles `R` and `R` Markdown files in the package directory.
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#' @inheritParams pkg_build
+#' @examples
+#' \dontrun{
+#' pkg_style(
+#'   pkg_root = "~/boilerplatePackage",
+#'   par = FALSE
+#' )
+#' }
+#' @importFrom jeksterslabRutils util_style
+#' @export
+pkg_style <- function(pkg_root = getwd(),
+                      par = TRUE,
+                      ncores = NULL) {
+  message(
+    "Styling..."
+  )
+  tryCatch(
+    {
+      util_style(
+        dir = pkg_root,
+        recursive = TRUE,
+        par = par,
+        ncores = ncores
+      )
+    },
+    error = function(err) {
+      warning(
+        "Error in styling."
+      )
+    }
+  )
+}
